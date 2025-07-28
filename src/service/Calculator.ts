8acbd54eac7905e00f885e92e51a2eccac4e754f
srgsuh <srg.suhoverhov@gmail.com>
2025-07-28 15:45:00 +0300

    Implement Calculator service

--- src/service/Calculator.ts
+++ src/service/Calculator.ts
@@ -13,8 +13,26 @@ type Response = Request & {
     result: number;
 };
 
+type Operation = Request["operation"];
+type OperationMap = {
+    [key in Operation]: (a: number, b: number) => number;
+}
+
+const MAPPER: OperationMap = {
+    "+": (a: number, b: number) => a + b,
+    "-": (a: number, b: number) => a - b,
+    "*": (a: number, b: number) => a * b,
+    "/": (a: number, b: number) => a / b,
+}
+
 export class Calculator implements Service{
     compute(bodyObject: unknown): string {
-        return "";
+        const request = requestSchema.parse(bodyObject);
+        const result = this._apply(request);
+        const response: Response = {...request, result};
+        return JSON.stringify(response);
+    }
+    _apply({first, second, operation}: Request): number {
+        return MAPPER[operation](first, second);
     }
 }
