f72f5ef4ba53966b9190925e029db060984c6df8
srgsuh <srg.suhoverhov@gmail.com>
2025-07-29 11:23:53 +0300

    Cleaning up unnecessary imports

--- src/controller/notFoundController.ts
+++ src/controller/notFoundController.ts
@@ -1,5 +1,5 @@
 import {IncomingMessage, ServerResponse} from "node:http";
-import {sendError} from "../utils/http-utils.js";
+import {sendError} from "../utils/http-utils.ts";
 
 export async function notFoundController(_: IncomingMessage, res: ServerResponse) {
     sendError(res, "Page not found", 404);
