6f69246d7bd265cffb6017347c3d414259f552c4
srgsuh <srg.suhoverhov@gmail.com>
2025-07-29 17:35:09 +0300

    Refactor calcController and Calculator service to execute validation in Controller

--- src/controller/calcController.ts
+++ src/controller/calcController.ts
@@ -1,16 +1,65 @@
 import {IncomingMessage, ServerResponse} from "node:http";
-import {Calculator} from "../service/Calculator.ts";
+import {Calculator, CalculatorError} from "../service/Calculator.ts";
 import {getRequestBody, sendError, sendSuccess} from "../utils/http-utils.ts";
+import z from "zod";
+import logger from "../logger.ts";
+import createError from "http-errors";
 
 const CALC = new Calculator();
 
+const requestSchema = z.object({
+    operation: z.string({message: "field 'operation' must a string"}),
+    first: z.number({message: "field 'first' must be a number"}),
+    second: z.number({message: "field 'second' must be a number"}),
+});
+
+type Request = z.infer<typeof requestSchema>;
+
 export async function calcController(req: IncomingMessage, res: ServerResponse) {
-    processRequest(req, CALC)
+    extractRequest(req)
+        .then(request => calculate(request, CALC))
         .then(computationResult => sendSuccess(res, computationResult))
         .catch(err => {sendError(res, err)});
 }
 
-async function processRequest(req: IncomingMessage, calculator: Calculator) {
+function calculate(request: Request, calculator: Calculator) {
+    try {
+        return calculator.compute(request);
+    }
+    catch (err: unknown) {
+        if (err instanceof CalculatorError) {
+            throw createError.BadRequest(err.message);
+        }
+        throw err;
+    }
+}
+
+async function extractRequest(req: IncomingMessage): Promise<Request> {
     const body = await getRequestBody(req);
-    return calculator.compute(body);
+    return await getRequestFromBody(body);
+}
+
+const FormatDescription = "Expected JSON object with fields: 'operation'(string), 'first'(number), and 'second'(number)"
+
+async function getRequestFromBody(bodyObject: unknown): Promise<Request> {
+    try {
+        const data = await requestSchema.parseAsync(bodyObject);
+        logger.debug(`calcController: Valid request received: ${JSON.stringify(data)}`);
+        return data;
+    }
+    catch (err: unknown) {
+        if (err instanceof z.ZodError) {
+            const formatError = getZodErrorMessages(err);
+            logger.debug(`calcController: Request validation failed: ${formatError}`);
+            const message = `Invalid request format: ${formatError}. ${FormatDescription}.`;
+            throw createError.BadRequest(message);
+        }
+        throw err;
+    }
+}
+
+function getZodErrorMessages(zodError: z.ZodError): string {
+    return zodError.issues
+        .map(issue => issue.message)
+        .join("; ");
 }
--- src/service/Calculator.ts
+++ src/service/Calculator.ts
@@ -1,20 +1,17 @@
-import z from "zod";
 import logger from "../logger.ts";
 
-const requestSchema = z.object({
-    operation: z.enum(["+", "-", "*", "/"], {message: "Field 'operation' must be one of the following: +, -, *, /"}),
-    first: z.number({message: "Field 'first' must be a number"}),
-    second: z.number({message: "Field 'second' must be a number"}),
-});
+export type CalcRequest = {
+    operation: string;
+    first: number;
+    second: number;
+};
 
-type CalcRequest = z.infer<typeof requestSchema>;
+export type CalcResponse = CalcRequest & {result: number;};
 
-type CalcResponse = CalcRequest & {result: number;};
-
-type Operation = CalcRequest["operation"];
+export class CalculatorError extends Error {}
 
 type OperationMap = {
-    [key in Operation]: (a: number, b: number) => number;
+    [operation: string]: (a: number, b: number) => number;
 }
 
 const MAPPER: OperationMap = {
@@ -25,36 +22,25 @@ const MAPPER: OperationMap = {
 }
 
 export class Calculator {
-    async compute (bodyObject: unknown): Promise<CalcResponse> {
-        const calcRequest = await this._getCalcRequest(bodyObject);
+    compute (calcRequest: CalcRequest): CalcResponse {
+        this._validate(calcRequest);
         const result = this._calculate(calcRequest);
         const response: CalcResponse = {...calcRequest, result};
         logger.debug(`Calculator: response: ${JSON.stringify(response)}`);
 
         return response;
     }
-    async _getCalcRequest(bodyObject: unknown): Promise<CalcRequest> {
-        try {
-            const data = await requestSchema.parseAsync(bodyObject);
-            logger.debug(`Calculator: Valid request received: ${JSON.stringify(data)}`);
-            return data;
+
+    _validate({operation, second}: CalcRequest): void {
+        if (!(operation in MAPPER)) {
+            throw new CalculatorError(`Unsupported operation: ${operation}. Expected one of: ${Object.keys(MAPPER).join(", ")}`);
         }
-        catch (err: unknown) {
-            logger.debug(`Calculator: Error: ${err}`);
-            if (err instanceof z.ZodError) {
-                const message = this._getZodErrorMessages(err);
-                logger.debug(`Calculator: Request validation failed: ${message}`);
-                throw new Error(message);
-            }
-            throw err;
+        if (operation === "/" && second === 0) {
+            throw new CalculatorError(`Division by zero is not supported.`);
         }
     }
+
     _calculate({first, second, operation}: CalcRequest): number {
         return MAPPER[operation](first, second);
-    }
-    _getZodErrorMessages(zodError: z.ZodError): string {
-        return zodError.issues
-            .map(issue => issue.message)
-            .join("; ");
     }
 }
